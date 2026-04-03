537749f879e70b84f2b61fff1c8099207acf4c00
Sayali Patil <sayalipatil228@gmail.com>
2026-04-03 17:47:00 +0530

    Test Added for password fields are masked

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # pages/registration_page.py
 # =====================
 # Page Object for the Registration Page.
@@ -26,13 +25,12 @@ class RegistrationPage(BasePage):
         self.chk_policy = page.locator('input[name="agree"]')
         self.radio_newsletter_yes = page.locator('input[name="newsletter"][value="1"]')
         self.radio_newsletter_no = page.locator('input[name="newsletter"][value="0"]')
-        self.btn_continue = page.locator(".btn-primary")
+        self.btn_continue = page.locator('input[value="Continue"]')  # from incoming
         self.msg_confirmation = page.locator('h1:has-text("Your Account Has Been Created!")')
         self.lbl_page_heading = page.get_by_role("heading", name="Register Account")
         self.msg_privacy_policy_warning = page.locator(".alert-danger")
         self.lnk_breadcrumb = page.locator("#account-register ul.breadcrumb")
 
-
         # ===== Error Message Locators =====
         self.err_privacy_policy = page.locator("div.alert-danger")
         self.err_firstname = page.locator("#input-firstname + .text-danger")
@@ -45,101 +43,6 @@ class RegistrationPage(BasePage):
 
         # ===== Warning / Validation Message Locators =====
         self.warn_privacy_policy = page.locator(".alert-danger")
-
-        # ===== Error Message Locators =====
-        self.err_privacy_policy = page.locator("div.alert-danger")
-        self.err_firstname = page.locator("#input-firstname + .text-danger")
-        self.err_lastname = page.locator("#input-lastname + .text-danger")
-        self.err_email = page.locator("#input-email + .text-danger")
-        self.err_telephone = page.locator("#input-telephone + .text-danger")
-        self.err_password = page.locator("#input-password + .text-danger")
-        self.password_mismatch_error = page.get_by_text(Message.password_not_match_error)
-        self.err_email_already_exist = page.get_by_text(Message.email_already_exist_error)
-
-        # ===== Warning / Validation Message Locators =====
-        self.warn_privacy_policy = page.locator(".alert-danger")
-
-    # ===== Action Methods =====
-
-    def set_first_name(self, fname: str):
-        """Enter the user's first name."""
-        self.fill(self.txt_firstname, fname)
-
-    def set_last_name(self, lname: str):
-        """Enter the user's last name."""
-        self.fill(self.txt_lastname, lname)
-
-    def set_email(self, email: str):
-        """Enter the user's email address."""
-        self.fill(self.txt_email, email)
-
-    def set_telephone(self, tel: str):
-        """Enter the user's telephone number."""
-        self.fill(self.txt_telephone, tel)
-
-    def set_password(self, pwd: str):
-        """Enter the password."""
-        self.fill(self.txt_password, pwd)
-
-    def set_confirm_password(self, pwd: str):
-        """Re-enter the password in the Confirm Password field."""
-        self.fill(self.txt_confirm_password, pwd)
-
-    def set_privacy_policy(self):
-        """Select the Privacy Policy checkbox."""
-        self.check(self.chk_policy)
-
-    def set_newsletter_subscription(self, locator: str | Locator):
-        self.check(locator)
-
-    def click_continue(self):
-        """Click the Continue button to submit the registration form."""
-        self.click(self.btn_continue)
-
-    def get_confirmation_msg(self):
-        """Return the confirmation message locator."""
-        return self.msg_confirmation
-
-    # ===== Combined Workflow =====
-
-    def complete_registration(self, user_data: dict):
-        """Complete the full registration process using a data dictionary."""
-        self.set_first_name(user_data["firstName"])
-        self.set_last_name(user_data["lastName"])
-        self.set_email(user_data["email"])
-        self.set_telephone(user_data["telephone"])
-        self.set_password(user_data["password"])
-        self.set_confirm_password(user_data["password"])
-        self.set_privacy_policy()
-        self.click_continue()
-        return self.msg_confirmation
-=======
-# pages/registration_page.py
-# =====================
-# Page Object for the Registration Page.
-# Inherits from BasePage for reusable UI interaction methods.
-
-from playwright.sync_api import Page
-
-from pages.base_page import BasePage
-
-
-class RegistrationPage(BasePage):
-    """Page Object Model class for the Registration Page."""
-
-    def __init__(self, page: Page):
-        super().__init__(page)
-
-        # ===== Locators =====
-        self.txt_firstname = page.locator("#input-firstname")
-        self.txt_lastname = page.locator("#input-lastname")
-        self.txt_email = page.locator("#input-email")
-        self.txt_telephone = page.locator("#input-telephone")
-        self.txt_password = page.locator("#input-password")
-        self.txt_confirm_password = page.locator("#input-confirm")
-        self.chk_policy = page.locator('input[name="agree"]')
-        self.btn_continue = page.locator('input[value="Continue"]')
-        self.msg_confirmation = page.locator('h1:has-text("Your Account Has Been Created!")')
 
     # ===== Action Methods =====
 
@@ -171,6 +74,9 @@ class RegistrationPage(BasePage):
         """Select the Privacy Policy checkbox."""
         self.chk_policy.check()
 
+    def set_newsletter_subscription(self, locator: str | Locator):
+        self.check(locator)
+
     def click_continue(self):
         """Click the Continue button to submit the registration form."""
         self.btn_continue.click()
@@ -187,6 +93,13 @@ class RegistrationPage(BasePage):
         """Return the privacy policy warning message locator."""
         return self.page.locator("#account-register > div.alert.alert-danger.alert-dismissible")
 
+    def get_password_field_type(self):
+        """Return the type attribute of the password field."""
+        return self.txt_password.get_attribute("type")
+
+    def get_confirm_password_field_type(self):
+        """Return the type attribute of the confirm password field."""
+        return self.txt_confirm_password.get_attribute("type")    
 
     # ===== Combined Workflow =====
 
@@ -201,6 +114,3 @@ class RegistrationPage(BasePage):
         self.set_privacy_policy()
         self.click_continue()
         return self.msg_confirmation
-
-
->>>>>>> 508fadd (Test Added without privacy policy registeration is not completed)
