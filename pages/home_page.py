2c682d7fee03dc8341ef9138d1030e9f4d20e919
Sayali Patil <sayalipatil228@gmail.com>
2026-04-04 18:11:28 +0530

    Updated changes

--- pages/home_page.py
+++ pages/home_page.py
@@ -31,10 +31,12 @@ class HomePage(BasePage):
 
     def get_home_page_title(self) -> str:
         """Return the title of the Home Page."""
+        self.lnk_my_account.wait_for(state="visible")
         return self.get_title()
 
     def click_my_account(self):
         """Click on the 'My Account' link."""
+        self.lnk_my_account.wait_for(state="visible")
         self.click(self.lnk_my_account)
 
     def click_register(self):
@@ -75,16 +77,5 @@ class HomePage(BasePage):
         self.click(self.lnk_show_all_desktops)
 
     def open_home_page(self):
-        self.page.goto("https://tutorialsninja.com/demo/")
+        self.page.goto("/")
         self.page.wait_for_load_state("networkidle")
-
-    def click_my_account(self):
-        self.lnk_my_account.wait_for(state="visible")
-        self.lnk_my_account.click()
-
-    def click_register(self):
-        self.lnk_register.click()
-
-    def click_login(self):
-        self.lnk_login.click()
-
--- tests/ui/Register/TC_RF_021_register_without_privacy_policy_test.py
+++ tests/ui/Register/TC_RF_021_register_without_privacy_policy_test.py
@@ -9,16 +9,13 @@
 Expected Result:
 Warning message - 'Warning: You must agree to the Privacy Policy!' should be displayed
 """
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
 
 import pytest
 from playwright.sync_api import expect
 from pages.home_page import HomePage
 from pages.registration_page import RegistrationPage
 from utils.helpers import RandomDataUtil
+from utils.constants import UIRoutes
 from utils.messages import PRIVACY_POLICY_WARNING_MSG
 
 @pytest.mark.sanity
