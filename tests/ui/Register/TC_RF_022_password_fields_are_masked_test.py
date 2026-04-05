699fdb576a1bf001e682505d1d4c681908012598
Sayali Patil <sayalipatil228@gmail.com>
2026-04-05 22:54:38 +0530

    Updated changes

--- tests/ui/Register/TC_RF_022_password_fields_are_masked_test.py
+++ tests/ui/Register/TC_RF_022_password_fields_are_masked_test.py
@@ -11,26 +11,23 @@ import pytest
 from playwright.sync_api import expect
 from pages.home_page import HomePage
 from pages.registration_page import RegistrationPage
-from utils.constants import VALID_PASSWORD
-
+from utils.constants import TestData
 
 @pytest.mark.sanity
 def test_password_fields_are_masked(page):
 
     home_page = HomePage(page)
     registration_page = RegistrationPage(page)
+    VALID_PASSWORD = TestData.VALID_PASSWORD
 
     # Step 1 & 2
     home_page.click_my_account()
     home_page.click_register()
 
     # Step 3
-    registration_page.set_password(VALID_PASSWORD)
-    registration_page.set_confirm_password(VALID_PASSWORD)
+    registration_page.set_password(TestData.VALID_PASSWORD)
+    registration_page.set_confirm_password(TestData.VALID_PASSWORD)
 
-    # Validation: check input type is password (masked)
-    password_type = registration_page.get_password_field_type()
-    confirm_password_type = registration_page.get_confirm_password_field_type()
-
-    assert password_type == "password"
-    assert confirm_password_type == "password"
+    # ✅ Validation (updated)
+    expect(registration_page.txt_password).to_have_attribute("type", "password")
+    expect(registration_page.txt_confirm_password).to_have_attribute("type", "password")
