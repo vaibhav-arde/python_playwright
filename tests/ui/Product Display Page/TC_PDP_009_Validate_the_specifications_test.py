0d257c369f36acc72b823801e3ab3f53c04d5916
Sayali Patil <sayalipatil228@gmail.com>
2026-04-16 15:19:11 +0530

    change locaters

---
+++ tests/ui/Product Display Page/TC_PDP_009_Validate_the_specifications_test.py
@@ -0,0 +1,45 @@
+from playwright.sync_api import Page
+
+from pages.home_page import HomePage
+from pages.search_results_page import SearchResultsPage
+from pages.product_page import ProductPage
+from utils.constants import TestData
+from utils import messages
+
+
+def test_validate_the_specifications(page: Page):
+    """
+    Test Case ID: TC_PDP_009
+    Validate the specifications of the Product in the Product Display Page.
+    """
+    home_page = HomePage(page)
+    search_results_page = SearchResultsPage(page)
+    product_page = ProductPage(page)
+
+    # Step 1-3: Search and open the first product that exposes a Specification tab
+    selected_product_name = ""
+    for product_name in TestData.PRODUCTS_WITH_SPECIFICATION_TAB:
+        home_page.enter_product_name(product_name)
+        home_page.click_search()
+
+        product_in_results = search_results_page.is_product_exist(product_name)
+        assert product_in_results is not None, messages.SEARCH_RESULT_PRODUCT_NOT_FOUND.format(keyword=product_name)
+        expected_product_name = product_in_results.text_content().strip()
+        assert expected_product_name != "", messages.SEARCH_RESULT_PRODUCT_NAME_EMPTY
+        search_results_page.select_product(expected_product_name)
+
+        if product_page.lnk_specification_tab.count() > 0 and product_page.lnk_specification_tab.is_visible():
+            selected_product_name = expected_product_name
+            break
+
+    assert selected_product_name != "", messages.PDP_SPECIFICATION_TAB_NOT_FOUND_FOR_PRODUCTS.format(
+        products=", ".join(TestData.PRODUCTS_WITH_SPECIFICATION_TAB)
+    )
+
+    # Step 4 (ER-1): Open Specification tab and validate specifications
+    assert product_page.lnk_specification_tab.is_visible(), messages.PDP_SPECIFICATION_TAB_NOT_VISIBLE
+    product_page.click_specification_tab()
+    assert product_page.pnl_specification.is_visible(), messages.PDP_SPECIFICATION_PANEL_NOT_VISIBLE
+
+    specification_text = product_page.get_specification_text()
+    assert specification_text != "", messages.PDP_SPECIFICATION_TEXT_EMPTY
