f30b0b2209f66b9cea8cd1951bcfcd1f5002fe5b
Sayali Patil <sayalipatil228@gmail.com>
2026-04-24 00:04:39 +0530

    Implement and validate TC_ATC_002 add to cart from wishlist

--- pages/home_page.py
+++ pages/home_page.py
@@ -27,6 +27,7 @@ class HomePage(BasePage):
         self.lnk_show_all_desktops = page.get_by_role("link", name="Show AllDesktops")
         self.dropdown = page.locator("a.dropdown-toggle").filter(has_text="My Account")
         self.lnk_wishlist = page.locator("#wishlist-total")
+        self.lnk_shopping_cart = page.get_by_role("link", name="Shopping Cart").first
 
     # ===== Action Methods =====
 
@@ -80,6 +81,10 @@ class HomePage(BasePage):
     def click_wishlist(self):
         """Click on the 'Wish List' link."""
         self.click(self.lnk_wishlist)
+
+    def click_shopping_cart(self):
+        """Click on the 'Shopping Cart' link in the top bar."""
+        self.click(self.lnk_shopping_cart)
 
     def open_home_page(self):
         """Navigate to the home page."""
