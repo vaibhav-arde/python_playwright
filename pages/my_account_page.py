18eca9ecfcba0dc3c685077a21684fa09b66bdc3
prajyotbhopale <prajyot1818@gmail.com>
2026-04-23 16:32:01 +0530

    feat: implement TC_MAI_002 - validate navigation to My Account Information page via Edit Account right column

--- pages/my_account_page.py
+++ pages/my_account_page.py
@@ -34,6 +34,7 @@ class MyAccountPage(BasePage):
         self.lnk_right_column_password = page.locator("#column-right").get_by_role(
             "link", name="Password"
         )
+        self.lnk_edit_right_column = page.get_by_role("link", name="Edit Account")
 
     # ===== Page Validation Methods =====
 
@@ -81,3 +82,8 @@ class MyAccountPage(BasePage):
     def click_password_right_column(self):
         """Click on the 'Password' link in the right column."""
         self.click(self.lnk_right_column_password)
+
+    def click_edit_right_column_option(self) -> EditAccountPage:
+        """Click on 'Edit your account information' and return EditAccountPage instance."""
+        self.click(self.lnk_edit_right_column)
+        return EditAccountPage(self.page)
