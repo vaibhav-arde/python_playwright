25271a1550c5dd7f3f77052c9fb08a60fac4e58b
prajyotbhopale <prajyot1818@gmail.com>
2026-04-24 18:22:48 +0530

    use authenticated page fixture instead register user

--- tests/ui/My_Account_Information/TC_MAI_001_Validate_navigating_to_My_Account_Information_page_from_My_Account_page_test.py
+++ tests/ui/My_Account_Information/TC_MAI_001_Validate_navigating_to_My_Account_Information_page_from_My_Account_page_test.py
@@ -8,26 +8,23 @@ import pytest
 from playwright.sync_api import expect
 from pages.home_page import HomePage
 from pages.my_account_page import MyAccountPage
-from pages.login_page import LoginPage
 from pages.edit_account_page import EditAccountPage
 from utils.constants import UIRoutes
 
 
 @pytest.mark.ui
 def test_validate_navigating_to_my_account_information_page_from_my_account_page(
-    page, registered_user
+    authenticated_page,
 ):
+    page = authenticated_page
     home_page = HomePage(page)
-    login_page = LoginPage(page)
     my_account_page = MyAccountPage(page)
     edit_account_page = EditAccountPage(page)
 
-    # Step 1: login using the valid credentials to ensure active session and clean context
     home_page.click_my_account()
-    home_page.click_login()
-
-    login_page.login_user(registered_user)
+    home_page.click_my_account_option()
     expect(my_account_page.get_my_account_page_heading()).to_be_visible()
+
     my_account_page.click_edit_account_info()
 
     expect(edit_account_page.get_page_heading()).to_be_visible()
--- tests/ui/My_Account_Information/TC_MAI_002_Validate_navigating_to_My_Account_Information_page_using_Edit_Account_Right_column_option_test.py
+++ tests/ui/My_Account_Information/TC_MAI_002_Validate_navigating_to_My_Account_Information_page_using_Edit_Account_Right_column_option_test.py
@@ -7,24 +7,22 @@ import pytest
 from playwright.sync_api import expect
 
 from pages.home_page import HomePage
-from pages.login_page import LoginPage
 from pages.my_account_page import MyAccountPage
 from pages.edit_account_page import EditAccountPage
 from utils.constants import UIRoutes
 
 
 @pytest.mark.ui
-def test_edit_account_info_right_column(page, registered_user):
+def test_edit_account_info_right_column(authenticated_page):
+    page = authenticated_page
     home_page = HomePage(page)
-    login_page = LoginPage(page)
     my_account_page = MyAccountPage(page)
     edit_account_page = EditAccountPage(page)
 
     home_page.click_my_account()
-    home_page.click_login()
-
-    login_page.login_user(registered_user)
+    home_page.click_my_account_option()
     expect(my_account_page.get_my_account_page_heading()).to_be_visible()
+
     my_account_page.click_edit_right_column_option()
 
     expect(edit_account_page.get_page_heading()).to_be_visible()
